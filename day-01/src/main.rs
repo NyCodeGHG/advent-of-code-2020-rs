744ae1fa3f35ef01cdfbfc2a3448a4f029ccd239
NyCode <me@nycode.dev>
2021-11-11 19:10:00 +0100

    Add second solution for day one

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,17 +3,19 @@ use std::fs;
 
 fn main() {
     let inputs = read_input();
-    let first = match first_solution(&inputs) {
-        Some(n) => n.to_string(),
-        None => "No Solution".to_string(),
-    };
+    let first = solution_to_string(first_solution(&inputs));
     println!("First Solution: {}", first);
+    let second = solution_to_string(second_solution(&inputs));
+    println!("Second Solution: {}", second);
 }
 
-fn first_solution(input: &Vec<i32>) -> Option<i32> {
-    let mut pairs: HashMap<i32, i32> = HashMap::new();
+fn first_solution(input: &Vec<i64>) -> Option<i64> {
+    let mut pairs: HashMap<i64, i64> = HashMap::new();
     for a in input {
         for b in input {
+            if a == b {
+                continue;
+            }
             pairs.insert(a + b, a * b);
         }
     }
@@ -23,7 +25,32 @@ fn first_solution(input: &Vec<i32>) -> Option<i32> {
     }
 }
 
-fn read_input() -> Vec<i32> {
+fn second_solution(input: &Vec<i64>) -> Option<i64> {
+    let mut pairs: HashMap<i64, i64> = HashMap::new();
+    for a in input {
+        for b in input {
+            for c in input {
+                if a == b || b == c || a == c {
+                    continue;
+                }
+                pairs.insert(a + b + c, a * b * c);
+            }
+        }
+    }
+    match pairs.get(&2020) {
+        Some(n) => Some(*n),
+        None => None,
+    }
+}
+
+fn solution_to_string(solution: Option<i64>) -> String {
+    match solution {
+        Some(n) => n.to_string(),
+        None => String::from("No Solution"),
+    }
+}
+
+fn read_input() -> Vec<i64> {
     fs::read_to_string("inputs/day01.txt")
         .expect("Unable to read day 1 input")
         .lines()

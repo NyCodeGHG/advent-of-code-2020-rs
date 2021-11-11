54bcdba599b4007a4023ea248850ba06916b8fe5
NyCode <me@nycode.dev>
2021-11-11 18:47:49 +0100

    Refactor first solution

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,22 +3,33 @@ use std::fs;
 
 fn main() {
     let inputs = read_input();
+    let first = match first_solution(&inputs) {
+        Some(n) => n.to_string(),
+        None => "No Solution".to_string(),
+    };
+    println!("First Solution: {}", first);
+}
+
+fn first_solution(input: &Vec<i32>) -> Option<i32> {
     let mut pairs: HashMap<i32, i32> = HashMap::new();
-    for a in &inputs {
-        for b in &inputs {
+    for a in input {
+        for b in input {
             pairs.insert(a + b, a * b);
         }
     }
-    println!("Solution One: {}", match pairs.get(&2020) {
-        Some(n) => n.to_string(),
-        None => "No solution".to_string()
-    })
+    match pairs.get(&2020) {
+        Some(n) => Some(*n),
+        None => None,
+    }
 }
 
 fn read_input() -> Vec<i32> {
     fs::read_to_string("inputs/day01.txt")
         .expect("Unable to read day 1 input")
         .lines()
-        .map(|line| line.parse().expect(&format!("Unable to parse {} into a number", &line)))
+        .map(|line| {
+            line.parse()
+                .expect(&format!("Unable to parse {} into a number", &line))
+        })
         .collect()
 }

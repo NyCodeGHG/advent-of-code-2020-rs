36aebf7ec4872dd50f9187828fb70cd872b168a4
NyCode <me@nycode.dev>
2021-11-13 22:12:22 +0100

    Add tests for day 03

--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -19,7 +19,7 @@ impl CellState {
     pub fn to_char(self) -> char {
         match self {
             CellState::EMPTY => '.',
-            CellState::TREE => '#'
+            CellState::TREE => '#',
         }
     }
 }
@@ -54,4 +54,43 @@ fn parse_map(text: &str) -> Vec<Vec<CellState>> {
         .map(|line| line.chars())
         .map(|chars| chars.map(|char| CellState::from_char(&char)).collect())
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_map() -> Vec<Vec<CellState>> {
+        parse_map(
+            "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#",
+        )
+    }
+
+    #[test]
+    fn test_first_solution() {
+        assert_eq!(find_trees(&test_map(), 3, 1), 7);
+    }
+
+    #[test]
+    fn test_second_solution() {
+        let map = &&test_map();
+        let result: u32 = vec![
+            find_trees(&map, 1, 1),
+            find_trees(&map, 3, 1),
+            find_trees(&map, 5, 1),
+            find_trees(&map, 7, 1),
+            find_trees(&map, 1, 2),
+        ].iter().product();
+        assert_eq!(result, 336);
+    }
 }
